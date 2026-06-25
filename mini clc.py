d649be884436ba9c64d1776bf24fb0420d0508bf
Lukman <lukmanmemon169@gmail.com>
2026-06-25 17:13:24 +0530

    Make a calculator
    python

---
+++ mini clc.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.title("Mini Calculator")
+
+n1 = st.number_input("Enter n1")
+n2 = st.number_input("Enter n2")
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    Sum = n1 + n2
+with col2:
+    sub = st.button("sub")
+with col3:
+    mul = st.button("mul")
+with col4:
+    Div = st.button("Div")
+if Sum:
+    ans = n1 + n2
+    st.text("Sum is {}".format(ans))
+if Sub:
+    ans = n1 - n2 
+    st.text("Subtration is {}".format(ans))
+if mul :
+    ans = n1 * n2
+    st.text("Multiplication is {}".format(ans))
+if div:
+    ans = n1 / n2
+    st.text("Division is {}".format(ans))               
