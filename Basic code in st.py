3afba377f653bc92d4dce0fbe02333c3a3a383f8
Lukman <lukmanmemon169@gmail.com>
2026-06-25 16:05:20 +0530

    Streamlit
    First try in Streamlit

---
+++ Basic code in st.py
@@ -0,0 +1,15 @@
+import streamlit as st
+# title
+st.title("Hello word")
+
+# header
+st.header("Hello Guys")
+
+# Header 
+st.header("This is a header")
+
+# Subheader
+st.subheader("This is Subheader")
+
+# Text
+st.text("Welcome to to the My Firs project ")
